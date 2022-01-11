19dba0796357cc15bcee4c96e6fd03202ca43511
Jeffkimerson <85892155+Jeffkimerson@users.noreply.github.com>
2022-01-11 10:16:54 -0800

    Add basic calculator operations

---
+++ script.js
@@ -0,0 +1,98 @@
+
+let num1;
+let num2;
+let temp = '';
+let tempOperator = ''
+let display = document.querySelector('.display');
+const number = document.querySelectorAll('button.number');
+number.forEach((button) => {
+  button.addEventListener('click', () => {
+    temp += button.innerHTML;
+    display.innerHTML = temp;
+  });
+});
+
+const operator = document.querySelectorAll('button.operator');
+operator.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (temp == "") {
+      if (tempOperator == '') {
+        tempOperator = button.innerHTML;
+      }
+      return;
+    }
+
+    else {
+
+    
+      if (num1 == undefined) {
+        num1 = temp; 
+        temp = '';
+        tempOperator = button.innerHTML;
+        console.log(tempOperator);
+      }
+      else if (tempOperator == '') {
+        tempOperator = button.innerHTML;
+      }
+      else {
+        temp = operate(num1, temp, tempOperator) 
+        num1 = '';
+        if (button.innerHTML == "=") {
+          tempOperator = '';
+        }
+        else {
+          tempOperator = button.innerHTML;
+        }
+        console.log(num1);
+      }
+      
+    }
+    
+  })
+})
+
+const operation = document.querySelectorAll('button.operation');
+operation.forEach((button) => {
+  button.addEventListener('click', () => {
+    let temp = button.innerHTML;
+    console.log(temp);
+  })
+})
+
+
+const operate = function(num1, num2, operator) {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+  let result;
+  switch (operator) {
+    case "/":
+      result = divide(num1, num2);
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "+":
+      result = add(num1, num2);
+      break;
+  }
+  return result;
+}
+
+const add = function(num1, num2) {
+  return num1 + num2;
+}
+
+const subtract = function(num1, num2) {
+  return num1 - num2;
+}
+
+const multiply = function(num1, num2) {
+  return num1 * num2;
+}
+
+const divide = function(num1, num2) {
+  return num1 / num2;
+}
